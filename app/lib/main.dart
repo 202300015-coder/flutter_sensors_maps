3e6e2889339a4ef477a3a1218ba6a12bed21efe0
202300015-Sergio <202300015@uprr.edu.mx>
2026-08-06 23:30:42 -0500

    no confio en este

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/route_optimizer_screen.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const RouteOptimizerScreen(),
     );
   }
 }
